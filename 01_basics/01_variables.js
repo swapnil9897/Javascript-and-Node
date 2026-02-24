6032e6ce555da29cf82d2cfc790ab4dca7de9d8e
Swapnil joshi <joshiswapnil30032003@gmail.com>
2026-02-24 11:03:21 +0530

    first commit

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,6 @@
+const accountId = 100;
+let accontEmail= "test@gmail.com";
+let accountName= "Regular";
+let accountLocation = "Deharadun";
+let custName = "Rohan";
+console.table([accountId ,custName, accontEmail , accountName , accountLocation]);
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,33 @@
+console.log("Swapnil");
+
+let name = "Swapnil";
+let age = 23;
+let isLoggedIn = false;
+
+console.table([name,age,isLoggedIn]); // for table structure with index values.
+console.table({name,age,isLoggedIn}); // for table structure with variable names.
+
+/*
+primitive data types:
+number ------- for nrmal integers
+bigint ------- for very big number
+string ------- for string
+boolean ------ for true or false values
+null --------  standalone value
+undefined ---- when a variable is left empty  the compiler automatically gives the output as undefined 
+symbol ------- to find uniqueness mostly used in react
+*/
+
+/*
+non-primitive data types:-
+
+typeof -------> it displays the type of data type a variable is
+
+console.log(typeof undefined); 
+output---> undefined
+
+console.log(typeof null);
+output---> object
+*/
+
+
